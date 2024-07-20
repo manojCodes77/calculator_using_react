0c2d9ae3dc7eda4527d4e0f83347612932b88d5d
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2024-07-20 10:02:05 +0530

    changes in vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/calculator_with_tailwind/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensure this matches your build directory
+  },
 })
